da77c1f6ffedf5f079ea34fe871e925a058d4951
Joe Conradt <joeconradt@wi.rr.com>
2014-11-17 12:55:28 -0600

    first commit

---
+++ exercise1.php
@@ -0,0 +1,35 @@
+<?php
+echo "Twinkle Twinkle Little Star <br/>";
+//Original phrase
+
+$Twinkle="Twinkle";
+$Star="Star";
+//Turned 'twinkle' and 'star' into variables
+
+echo "$Twinkle $Twinkle Little $Star <br/>";
+//Echoed the variables, and got the same message since they were named the same
+
+$Twinkle="Sparkle";
+$Star="Diamond";
+//Renamed the variables
+
+echo "$Twinkle $Twinkle Little $Star <br/>";
+//Got a message that spelled "Sparkle Sparkle Little Diamond"
+
+
+/*
+*
+*	EXERCISE 2
+*
+*/
+
+$x=10;
+$y=7;
+
+$equation1="$x" + "$y"
+
+
+
+
+
+?>
